16a0b0780edffb007644f1b4c46662a96b5529dc
mostafaelbeih <mostafa.elbeihjr@gmail.com>
2023-06-06 23:12:49 +0300

    edited unused projects and changed website theme

--- src/App.js
+++ src/App.js
@@ -10,34 +10,34 @@ function App() {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
 
-  // const fetchData = async () => {
-  //     const response = await fetch("http://10.23.68.229:8783/api/p2p-ebu-auth-service/login", {
-  //       method: 'POST',
-  //       body: JSON.stringify({
-  //         userName: "melbeih",
-  //         password: "Kendrick_lamar_222"
-  //       }),
-  //       headers: {
-  //         'Content-type': 'application/json'
-  //       },
-  //     })
-  //       .then(response => response.json())
-  //       .then(data => {
-  //         console.log(data);
-  //       })
-  //       .catch(err => {
-  //         console.log(err.message);
-  //       });
-  // }
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  const fetchData = async () => {
+      const response = await fetch("http://10.23.68.229:8783/api/p2p-ebu-auth-service/login", {
+        method: 'POST',
+        body: JSON.stringify({
+          userName: "melbeih",
+          password: "Kendrick_lamar_222"
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div
-      style={{
-        backgroundColor: darkMode ? "#222" : "#FEC260",
-        color: "#FEC260" && "#2A0944",
-      }}
+    style={{
+      backgroundColor: darkMode ? "#222" : "#F6F6F2",
+      color: "#F6F6F2" && "#388087",
+    }}
     >
       {/* <Toggle /> */}
       <Intro />
--- src/data.js
+++ src/data.js
@@ -1,33 +1,18 @@
 export const products = [
   {
     id: 1,
-    img: "../img/ecommerce.png",
-    link: "https://ecommerce-sanity-stripe-murex.vercel.app/",
-  },
-  {
-    id: 2,
-    img: "../img/crypto.png",
-    link: "https://crypto-argos.herokuapp.com/",
-  },
-  {
-    id: 3,
-    img: "../img/cesare.png",
-    link: "https://cesare-clothing-brand.herokuapp.com/",
-  },
-  {
-    id: 4,
     img: "../img/newjersey.png",
-    link: "https://newjerseydevelopment.herokuapp.com/",
+    link: "https://primedev-eg.com",
   },
   
   {
-    id: 5,
+    id: 2,
     img: "../img/prime-dev.png",
-    link: "https://primedev-eg.com",
+    link: "https://ramsesmedical.net/",
   },
   {
-    id: 6,
+    id: 3,
     img: "../img/ramsesmedical.png",
-    link: "https://ramsesmedical.net/",
+    link: "https://ecommerce-sanity-stripe-murex.vercel.app/",
   },
 ];
